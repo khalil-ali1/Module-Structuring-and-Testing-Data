be2283a362015bb397127002f175cee956224c45
khalil-ali1 <khalil.ali.uk1@gmail.com>
2024-11-19 09:47:01 +0000

    finish errors/0.js

--- Sprint-2/errors/0.js
+++ Sprint-2/errors/0.js
@@ -1,9 +1,12 @@
 // Predict and explain first...
+//str is declared twice.
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
 function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
+  return str = `${str[0].toUpperCase()}${str.slice(1)}`;
 }
+
+//to print the output to console :
+console.log(capitalise("hello"));
