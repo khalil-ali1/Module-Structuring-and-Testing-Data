d7c137027d60f8cb596474773b0680ad0c54c56e
khalil-ali1 <khalil.ali.uk1@gmail.com>
2024-11-19 06:29:06 +0000

    finish 1.js

--- Sprint-2/debug/1.js
+++ Sprint-2/debug/1.js
@@ -1,8 +1,7 @@
 // Predict and explain first...
 
 function sum(a, b) {
-  return;
-  a + b;
+  return (a + b); //there was no thing to return.(a+b) should be in the same line with "return"
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
