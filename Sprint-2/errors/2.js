b8832ea939772cb994ea049d1c78533b78f7435b
khalil-ali1 <khalil.ali.uk1@gmail.com>
2024-11-19 10:57:53 +0000

    finish errors/2.js

--- Sprint-2/errors/2.js
+++ Sprint-2/errors/2.js
@@ -1,10 +1,15 @@
 
 // Predict and explain first...
+//1-wrong function declaration,
+//2-no function call
 
 // this function should square any number but instead we're going to get an error
 
-function square(3) {
+function square(num) {
     return num * num;
 }
 
-
+console.log(square(3))
+console.log(square(5))
+console.log(square(8))
+//add function call
