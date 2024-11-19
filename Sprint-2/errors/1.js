fcb72cf3ba786beea84bf83574e1d1361d85cd29
khalil-ali1 <khalil.ali.uk1@gmail.com>
2024-11-19 10:00:41 +0000

    finish errors/1.js

--- Sprint-2/errors/1.js
+++ Sprint-2/errors/1.js
@@ -1,13 +1,13 @@
 // Predict and explain first...
+//declare decimalNumber outside the function before using it
+
+
 
 // Why will an error occur when this program runs?
 // Try playing computer with the example to work out what is going on
-
+const decimalNumber = 0.5;
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
-
   return percentage;
 }
-
-console.log(decimalNumber);
+console.log(convertToPercentage(decimalNumber));//wrong function call.
