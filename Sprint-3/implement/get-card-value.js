a7e6f6fd7490a97e7448bc75daa8d8f069d43c09
khalil-ali1 <khalil.ali.uk1@gmail.com>
2024-11-24 19:37:06 +0000

    finish get-card-value.js

--- Sprint-3/implement/get-card-value.js
+++ Sprint-3/implement/get-card-value.js
@@ -29,3 +29,20 @@
 // Given a card with an invalid rank (neither a number nor a recognized face card),
 // When the function is called with such a card,
 // Then it should throw an error indicating "Invalid card rank."
+
+function getCardValue(card){
+    const rank = card.slice(0, -1);
+    if (rank>=2 && rank<=9)
+        return Number(rank)
+    else if(rank==="10" || rank==="J"  || rank==="Q" || rank==="K")
+        return 10;
+    else if(rank==="A")
+        return 11;
+    else
+        return "Invalid card rank.";
+}
+console.log(getCardValue("7♠")); 
+console.log(getCardValue("A♦")); 
+console.log(getCardValue("K♣")); 
+console.log(getCardValue("10♥")); 
+console.log(getCardValue("X♠"));  
