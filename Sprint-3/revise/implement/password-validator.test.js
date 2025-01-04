a03fe8737463af4f9b54ba96c24c3a77b798fbb5
khalil-ali1 <khalil.ali.uk1@gmail.com>
2025-01-04 23:14:06 +0000

    password-validator

--- Sprint-3/revise/implement/password-validator.test.js
+++ Sprint-3/revise/implement/password-validator.test.js
@@ -14,3 +14,49 @@ To be valid, a password must:
 
 You must breakdown this problem in order to solve it. Find one test case first and get that working
 */
+function isValidPassword(password, previousPasswords= []) {
+    // Check length
+    if (password.length < 5) {
+      return false;
+    }
+  
+    // Check for uppercase, lowercase, number, and symbol
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    const hasSymbol = /[!#$%&.\*]/.test(password);
+  
+    if (!hasUppercase || !hasLowercase || !hasNumber || !hasSymbol) {
+      return false;
+    }
+  
+    // Check for uniqueness
+    if (previousPasswords.includes(password)) {
+      return false;
+    }
+  
+    return true;
+  }
+
+
+  test("password has less than 5 characters", () => {
+    expect(isValidPassword("aaaa")).toEqual(false);
+  });
+  test("password without an uppercase letter", () => {
+    expect(isValidPassword("abcdef")).toEqual(false);
+  });
+  test("password without a lowercase letter", () => {
+    expect(isValidPassword("ABCDEF")).toEqual(false);
+  });
+  test("password without a number", () => {
+    expect(isValidPassword("abcdef")).toEqual(false);
+  });
+  test("passwords without a non-alphanumeric symbol", () => {
+    expect(isValidPassword("abcdef")).toEqual(false);
+  });
+  test("password that have been used before", () => {
+    expect(isValidPassword("abcdef", ["abcdeA1!"])).toEqual(false);
+  });
+  test("valid passwords", () => {
+    expect(isValidPassword("abcdeA2!")).toEqual(true);
+  });
